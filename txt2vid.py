eeae880f2e0fb5a22700d29d53f26f8781351d23
trung-vt <trung.vuthanh24@gmail.com>
2024-03-09 21:06:42 +0000

    Extract sampling function

--- txt2vid.py
+++ txt2vid.py
@@ -169,6 +169,34 @@ def change_fps_if_required(value_dict):
         saving_fps = value_dict["fps"]
     return saving_fps
 
+def produce_sample(H, W, num_frames, C, F, options, model, value_dict, sampler, num_samples, decoding_t):
+    out = do_sample(
+                model,
+                sampler,
+                value_dict,
+                num_samples,
+                H,
+                W,
+                C,
+                F,
+                T=num_frames,
+                batch2model_input=["num_video_frames", "image_only_indicator"],
+                force_uc_zero_embeddings=options.get("force_uc_zero_embeddings", None),
+                force_cond_zero_embeddings=options.get(
+                    "force_cond_zero_embeddings", None
+                ),
+                return_latents=False,
+                decoding_t=decoding_t,
+            )
+
+    if isinstance(out, (tuple, list)):
+        samples, samples_z = out
+    else:
+        samples = out
+        samples_z = None
+    return samples
+
+
 if __name__ == "__main__":
 
     model_version = get_model_version()
@@ -227,30 +255,7 @@ if __name__ == "__main__":
         saving_fps = change_fps_if_required(value_dict)
 
         if st.button("Sample"):
-            out = do_sample(
-                model,
-                sampler,
-                value_dict,
-                num_samples,
-                H,
-                W,
-                C,
-                F,
-                T=num_frames,
-                batch2model_input=["num_video_frames", "image_only_indicator"],
-                force_uc_zero_embeddings=options.get("force_uc_zero_embeddings", None),
-                force_cond_zero_embeddings=options.get(
-                    "force_cond_zero_embeddings", None
-                ),
-                return_latents=False,
-                decoding_t=decoding_t,
-            )
-
-            if isinstance(out, (tuple, list)):
-                samples, samples_z = out
-            else:
-                samples = out
-                samples_z = None
+            samples = produce_sample(H, W, num_frames, C, F, options, model, value_dict, sampler, num_samples, decoding_t)
 
             if save_locally:
                 save_video_as_grid_and_mp4(samples, save_path, num_frames, fps=saving_fps)
